8db547041de36833880fea0f35e2b30c2f67797e
Anna Naumenko <anja.naymenko@gmail.com>
2022-05-04 12:47:24 +0300

    crate new file

---
+++ JS.HW_4.js
@@ -0,0 +1,17 @@
+let a=1
+let b=2                    //first 
+for ( b**a; a<11; a++){
+    console.log(b**a);
+}
+
+
+ function showNumber(a){
+    
+    let b=2                    //first *
+  while ( a<11 ){
+       console.log(b**a)
+       a++
+  }}
+   showNumber(1) 
+
+
